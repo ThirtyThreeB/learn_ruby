49603579da6345639bf63920f1698d1a16349627
Gabriel Aragon <thirtythreebananas@gmail.com>
2017-11-17 11:30:16 -0500

    Ex 03 solutions

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,34 @@
-#write your code here
+def echo(input)
+  input
+end
+
+def shout(input)
+  input.upcase
+end
+
+def repeat(input, n=2)
+  [input] * n * ' '
+end
+
+def start_of_word(input, n)
+  input[0, n]
+end  
+
+def first_word(input)
+  arr=input.split
+  arr[0]
+end
+
+def titleize input
+  arr=input.split
+  #arr[0].capitalize
+  arr.each do |word|
+    if word != 'and' && word != 'over' && word != 'the'
+      word.capitalize!
+    elsif word==arr[0]
+      word.capitalize!
+    end
+  end
+  arr.join(' ')
+end
+  
