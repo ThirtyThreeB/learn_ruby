964f45792019f17ef4b3688ae2c91a780a60c812
Gabriel Aragon <thirtythreebananas@gmail.com>
2017-11-17 11:33:21 -0500

    Ex 05 solutions

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,19 @@
 class Book
-# write your code here
+  def title
+    @title
+  end
+
+  def title=(title)
+    not_words = %w(and if a an the of in)
+    @title = title.split.each_with_index.map do |word, i|
+      if !not_words.include?(word) || i == 0
+        word.capitalize
+      else
+        word
+      end
+       #puts title ##why does a puts here affect the code??
+    end.join(" ")#what variable name is being returned here??
+#puts @title## also here??
+  end
 end
+
